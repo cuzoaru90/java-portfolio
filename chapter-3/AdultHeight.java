1da3b8eee34bbc1876d5e94d6bb2d40a2a154625
Chuck Uzoaru <chuckuzoaru@chucks-mbp.attlocal.net>
2017-11-22 23:23:55 -0600

    Halfway through Chapter 3 programs

---
+++ chapter-3/AdultHeight.java
@@ -0,0 +1,56 @@
+/* One way to estimate the adult height of a child is to use the following formula, which uses the height of the
+parents:
+
+H(male_child) = ( height_mother) x (13/12) + H(father) ) / 2
+H(female_child) = (  height_father) x (12/13) + H(mother) ) / 2
+
+All heights are in inches. Write a program that takes as input the gender of the child, the height of the mother in
+inches, and the height of the father in inches, and outputs the estimated adult height of the child in inches. The
+program should allow the user to enter a new set of values and output the predicted height until the user decides to
+exit. The user should be able to input the heights in feet and inches, and the program should output the estimated
+height of the child in feet and inches. Use the int data type to store the heights.
+
+ */
+
+import java.util.*;
+
+public class AdultHeight {
+  public static void main(String[]args){
+
+    int fatherHeight, motherHeight;
+    double childHeight = 0;
+    boolean programAlive = true;
+
+    String genderInput = "";
+
+    Scanner keyboard = new Scanner(System.in);
+
+    do {
+      System.out.println("Enter the gender of the child (m for male, f for female, q to quit)");
+      genderInput = keyboard.next().toLowerCase();
+
+      if (genderInput.equals("q")){
+         System.out.println("goodbye");
+         System.exit(0);
+      }
+
+      System.out.println("Enter the height of the mother (in feet and inches):");
+      motherHeight = keyboard.nextInt() * 12 + keyboard.nextInt();
+      System.out.println("Enter the height of the father (in feet and inches):");
+      fatherHeight = keyboard.nextInt() * 12 + keyboard.nextInt();
+
+      if (genderInput.equals("m")){
+        childHeight = (motherHeight * (13.0/12) + fatherHeight) / 2;
+      }
+      else if (genderInput.equals("f")){
+        childHeight = (fatherHeight * (12.0/13) + motherHeight) / 2;
+      }
+      else
+        System.out.println("Provided invalid gender, so height is undetermined");
+
+      System.out.printf("Estimated height of the child: %.2f inches\n\n", childHeight);
+
+    } while (programAlive);
+
+  }
+}
---
+++ chapter-3/Fibonacci.java
@@ -0,0 +1,57 @@
+/* The Fibonacci numbers F(n) are are defined as follows: F(0) is 1, F(1) is 1, and F(i+2) = F(i) + F(i+1)
+i = 0, 1, 2,...
+
+In other words, each number is the sum of the previous two numbers. The first few Fibonacci numbers are 1,1,2,3,5, and
+8. One place where these numbers occur is as certain population growth rates. If a population has no deaths, then the
+series shows the size of the population after each time period. It takes an organism two time periods to mature to
+reproducing age, and then the organism reproduces once every time period. The formula applies most straightforwardly
+to asexual reproduction at a rate of one offspring per time period. In any event, the green crud population grows at
+this rate and has a time period of 5 days. Hence, if a green crud population starts out as 10 pounds of crud, then in
+5 days, there is still 10 pounds of crud; in 10 days, there is 20 pounds of crud; in 15 days, 30 pounds; in 20 days,
+50 pounds; and so forth.
+
+Write a program that takes both the initial size of a green crud population (in pounds) and a number of days as input
+and outputs the number of pounds of green crud after that many days. Assume that the population size is the same for
+4 days and then increases every fifth day. Your program should allow the user to repeat this calculation as often as
+desired.
+
+ */
+import java.util.*;
+
+public class Fibonacci {
+  public static void main(String[] args){
+
+    int populationInput = 0, totalPopulation = 0, days = 0;
+    boolean programAlive = true;
+    String choice = "";
+    Scanner keyboard = new Scanner(System.in);
+
+    do {
+      System.out.println("Enter in the initial size of the green crud population (in pounds): ");
+      populationInput = keyboard.nextInt();
+
+      System.out.println("Enter the number of days over which the population will grow: ");
+      days = keyboard.nextInt();
+
+      int precedingTotal = 0;
+      int lastTotal = populationInput;
+
+      for (int i = 0; i <= days; i+=5 ){
+        totalPopulation = precedingTotal + lastTotal;
+        precedingTotal = lastTotal;
+        lastTotal = totalPopulation;
+      }
+
+      System.out.printf("Population after %d days is %d pounds\n", days, totalPopulation);
+      System.out.println("Do you want to do another calculation? Press q to quit.");
+      totalPopulation = 0;
+      choice = keyboard.next();
+
+      if ( choice.equals("q") )
+        programAlive = false;
+
+    } while (programAlive);
+    
+  }
+
+}
